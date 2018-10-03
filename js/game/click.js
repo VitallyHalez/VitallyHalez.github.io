3c63dae519e31f30371116d4647cd73b399b181b
Vitally <Vitally@DESKTOP-33HV0AB>
2018-10-03 17:48:17 +0300

    ws edit

--- js/game/click.js
+++ js/game/click.js
@@ -5,8 +5,6 @@ document.addEventListener("click", function(event) {
     if (event.target.classList.contains("box")) {
         game.add(event.target.id);
         
-        
-
         render(game);
         
         temp.innerText += "Clicked " + player + ":" + event.target.id + "\n";
@@ -32,7 +30,6 @@ function render(game){
         box = document.querySelector(`#${item}`)
         box.innerText = "x";
     });
-    myArr = Array.from(game)
     
-    socket.send(JSON.stringify({game: JSON.stringify(myArr), method:'render'}))
+    socket.send(JSON.stringify({game: JSON.stringify(Array.from(game)), method:'render'}))
 }
