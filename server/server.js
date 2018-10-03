0de1ebf97b1dbd308751cbe8072423184e8826bd
Vitally <Vitally@DESKTOP-33HV0AB>
2018-10-03 17:34:52 +0300

    Edit file folder

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var socket = new WebSocket("wss://node2.wsninja.io");
+
+var handleMessage; 
+
+socket.addEventListener('open', function(event) {
+    socket.send(JSON.stringify({ guid: "3b67a2fa-0dfa-4de5-b597-7edddbfa4a9f" }));
+});
+
+socket.addEventListener('message', function(event) {
+    handleMessage(JSON.parse(event.data));
+});
+
+function restartGame() {
+    location.href = "game.html";
+    socket.send(JSON.stringify( { method: "restart" } ));
+}
+
+function handleMessage(message) {
+    if (message.method === "restart") {
+        location.href = "game.html";
+    }
+
+    if (message.method === "render") {
+        render(game);
+    }
+}
