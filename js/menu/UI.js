f95cc807573d52f01f1b310209c2c7c74e8584f3
Vitally <Vitally@DESKTOP-33HV0AB>
2018-10-03 21:30:28 +0300

    Mobile beta

--- js/menu/UI.js
+++ js/menu/UI.js
@@ -1,7 +1,10 @@
 enter.addEventListener('click', function(){
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        location.href="mobilegame.html"; 
-    } else {
-        location.href="game.html"; 
-    }
+    location.href="game.html"; 
 })
+// enter.addEventListener('click', function(){
+//     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//         location.href="mobilegame.html"; 
+//     } else {
+//         location.href="game.html"; 
+//     }
+// })
