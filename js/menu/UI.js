2a7cb80e7a3f98c7df1398f9833d5d5e07475068
Vitally <Vitally@DESKTOP-33HV0AB>
2018-10-03 21:03:48 +0300

    Add mobile beta

--- js/menu/UI.js
+++ js/menu/UI.js
@@ -1,3 +1,7 @@
 enter.addEventListener('click', function(){
-    location.href="game.html"; 
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        location.href="mobilegame.html"; 
+    } else {
+        location.href="game.html"; 
+    }
 })
