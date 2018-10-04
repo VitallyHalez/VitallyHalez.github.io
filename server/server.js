1ab2c5a75e552def0cb4f30e8516ab4b5d3a9e34
Vitally <Vitally@DESKTOP-33HV0AB>
2018-10-04 12:34:47 +0300

    backup

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var socket = new WebSocket("wss://node2.wsninja.io");
 var handleMessage; 
 
 socket.addEventListener('open', function(event) {
-    socket.send(JSON.stringify({ guid: "maxpavle-kofa-4de5-b597-7edddbfa4a88" }));//9f 3b67a2fa-0dfa-4de5-b597-7edddbfa4a88
+    socket.send(JSON.stringify({ guid: "3b67a2fa-0dfa-4de5-b597-7edddbfa4a88" }));//9f 3b67a2fa-0dfa-4de5-b597-7edddbfa4a88
 });
 
 socket.addEventListener('message', function(event) {
