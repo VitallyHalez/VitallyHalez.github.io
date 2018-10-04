d84226955ab0590dc7155130eef16c5bab3a28b8
Vitally <Vitally@DESKTOP-33HV0AB>
2018-10-04 13:58:28 +0300

    Edit UI menu

--- js/game/UI.js
+++ js/game/UI.js
@@ -2,6 +2,10 @@ restartBtn.addEventListener('click', function(){
     restartGame(); 
 })
 
+menuBtn.addEventListener('click', function(){
+    location.href="index.html"
+})
+
 window.addEventListener("beforeunload", function (event) {   
     socket.send(JSON.stringify({method:'closed'}))
 });
